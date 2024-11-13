6c55ed3ca6c5204d6657067be7fb263ba4f39c9f
DoDucDac <im3d.11399@gmail.com>
2024-11-13 20:55:32 +0700

    init

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+const tooltipTriggerList = document.querySelectorAll(
+	'[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+	tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var wScroll = $(this).scrollTop();
+		if (wScroll >= 80) {
+			$('.mf-header').addClass('sticky');
+		} else {
+			$('.mf-header').removeClass('sticky');
+		}
+	});
+
+	if ($(window).scrollTop() >= 80) {
+		$('.mf-header').addClass('sticky');
+	} else {
+		$('.mf-header').removeClass('sticky');
+	}
+
+	$('.mf-banner-slide').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		infinite: true,
+		autoPlay: true,
+		autoplaySpeed: 2000
+	});
+	$('.mf-verse-slide').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		asNavFor: '.mf-verse-slide-nav'
+	});
+	$('.mf-verse-slide-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.mf-verse-slide',
+		dots: false,
+		arrows: true,
+		focusOnSelect: true,
+		centerMode: true,
+		centerPadding: '0px'
+	});
+	$('.mf-oawbmp-slide').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		infinite: true,
+		autoPlay: true,
+		autoplaySpeed: 2000
+	});
+	$('.mf-oawmeig-slide').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: '120px',
+		dots: true,
+		arrows: false,
+		infinite: true,
+		autoPlay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('#PlayVideo').on('click', () => {
+		$('#videoModal iframe').attr(
+			'src',
+			'https://www.youtube.com/embed/XF8UbB5LhFw?si=qgJ0BOu_T0kLbhuY?autoplay=1&enablejsapi=1'
+		);
+		var myModalEl = document.getElementById('videoModal');
+		var videoModal = new bootstrap.Modal(myModalEl, {});
+		videoModal.show();
+		myModalEl.addEventListener('hidden.bs.modal', function (event) {
+			$('#videoModal iframe').attr('src', '');
+		});
+	});
+});
