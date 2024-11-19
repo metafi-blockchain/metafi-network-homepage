fb5c91e1ddbbedefe35d7882adf1749ab902e400
DoDucDac <im3d.11399@gmail.com>
2024-11-19 19:48:16 +0700

    update

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -27,40 +27,28 @@ $(document).ready(function () {
 
 	const getActorSlide = actorId => `
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
+      <button type="button" class="play" id="PlayVideo-slide-${actorId}" tabindex="0">
         <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
+      </button> 
       <img src="assets/images/metafi_verse_${3 * actorId - 2}.png" alt="" />
     </div>
 	
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
-        <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
       <img src="assets/images/metafi_verse_${3 * actorId - 2 + 1}.png" alt="" />
     </div>
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
-        <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
       <img src="assets/images/metafi_verse_${3 * actorId - 2 + 2}.png" alt="" />
     </div>
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
+	 <button type="button" class="play" id="PlayVideo-slide-${actorId}" tabindex="0">
         <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
+      </button> 
       <img src="assets/images/metafi_verse_${3 * actorId - 2}.png" alt="" />
     </div>
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
-        <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
       <img src="assets/images/metafi_verse_${3 * actorId - 2 + 1}.png" alt="" />
     </div>
     <div id="actor-${actorId}" class="mf-verse-slide-item">
-      <button type="button" class="play" id="PlayVideo" tabindex="0">
-        <img src="assets/images/icons/play-video.svg" alt="">
-      </button>
       	<img src="assets/images/metafi_verse_${3 * actorId - 2 + 2}.png" alt="" />
     </div>
   `;
@@ -84,24 +72,65 @@ $(document).ready(function () {
       <img src="assets/images/metafi_verse_${3 * actorId - 2 + 2}.png" alt="" />
     </div>
   `;
+	// Initialize verse slides with first actor
 	$('.mf-verse-slide').html(getActorSlide(1));
 	$('.mf-verse-slide-nav').html(getActorNav(1));
-	$('.mf-verse-tabs li').on('click', function (e) {
+
+	// Video URL mapping
+	const videoUrls = {
+		'1': 'https://www.youtube.com/embed/0T34-WHJ8d0?si=757-gN07rc2ziHZk&autoplay=1&enablejsapi=1',
+		'2': 'https://www.youtube.com/embed/kNIDk5dQsJU?si=FUoOhm7T9VKU6FkK&autoplay=1&enablejsapi=1', 
+		'3': 'https://www.youtube.com/embed/0T34-WHJ8d0?si=757-gN07rc2ziHZk&autoplay=1&enablejsapi=1',
+		'4': 'https://www.youtube.com/embed/RK_45Z-2QC4?si=SOllsKfObeEOeUpE&autoplay=1&enablejsapi=1',
+		'5': 'https://www.youtube.com/embed/XF8UbB5LhFw?si=qgJ0BOu_T0kLbhuY?autoplay=1&enablejsapi=1',
+		'6': 'https://www.youtube.com/embed/XF8UbB5LhFw?si=qgJ0BOu_T0kLbhuY?autoplay=1&enablejsapi=1',
+		'7': 'https://www.youtube.com/embed/XF8UbB5LhFw?si=qgJ0BOu_T0kLbhuY?autoplay=1&enablejsapi=1'
+	};
+
+	// Handle video play button clicks
+	const handleVideoPlay = (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+		event.currentTarget.style.display = 'none';
+
+		const slideId = event.currentTarget.parentElement.id.split('-')[1];
+		const videoUrl = videoUrls[slideId] || videoUrls['1'];
+
+		const iframe = document.createElement('iframe');
+		iframe.setAttribute('allow', 'autoplay');
+		iframe.src = videoUrl;
+		iframe.className = event.currentTarget.parentElement.lastElementChild.className;
+		event.currentTarget.parentElement.lastElementChild.replaceWith(iframe);
+	};
+
+	// Attach video play handlers
+	for (let i = 1; i <= 7; i++) {
+		$(`#PlayVideo-slide-${i}`).on('click', handleVideoPlay);
+	}
+
+	// Handle verse tab clicks
+	$('.mf-verse-tabs li').on('click', function(e) {
 		e.preventDefault();
-		// get the actor item
-		const actorItem = $(e.target).closest('li').length
-			? $(e.target).closest('li')
-			: $(e.target).is('li')
-			? $(e.target)
-			: null;
+
+		const actorItem = $(e.target).closest('li').length ? 
+			$(e.target).closest('li') : 
+			$(e.target).is('li') ? $(e.target) : null;
+
+		// Update active state
 		actorItem.siblings().removeClass('active');
 		actorItem.addClass('active');
+
 		const actorId = actorItem.attr('id').split('-')[1];
+
+		// Update images
 		$('#verse-image').attr('src', `assets/images/verse-${actorId}-image.png`);
-		$('.mf-verse-slide').slick('unslick');
-		$('.mf-verse-slide-nav').slick('unslick');
-		$('.mf-verse-slide').empty().html(getActorSlide(actorId));
-		$('.mf-verse-slide-nav').empty().html(getActorNav(actorId));
+		$('#verse-bg').attr('src', `assets/images/metafi_verse${actorId}.png`);
+
+		// Reinitialize slides
+		$('.mf-verse-slide').slick('unslick').empty().html(getActorSlide(actorId));
+		$('.mf-verse-slide-nav').slick('unslick').empty().html(getActorNav(actorId));
+
+		// Configure main slide
 		$('.mf-verse-slide').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
@@ -110,6 +139,8 @@ $(document).ready(function () {
 			infinite: true,
 			asNavFor: '.mf-verse-slide-nav'
 		});
+
+		// Configure nav slide  
 		$('.mf-verse-slide-nav').slick({
 			infinite: true,
 			slidesToShow: 3,
@@ -121,7 +152,11 @@ $(document).ready(function () {
 			centerMode: true,
 			centerPadding: '0px'
 		});
-		$('#verse-bg').attr('src', `assets/images/metafi_verse${actorId}.png`);
+
+		// Reattach video handlers
+		for (let i = 1; i <= 7; i++) {
+			$(`#PlayVideo-slide-${i}`).on('click', handleVideoPlay);
+		}
 	});
 
 	$('.mf-banner-slide').slick({
@@ -215,6 +250,7 @@ $(document).ready(function () {
 			$('#videoModal iframe').attr('src', '');
 		});
 	});
+
 	const setMenuMb = () => {
 		if (window.innerWidth > 1199) {
 			$('#main-menu').removeClass('show');
