7d041ff0f764c1c0bbe8005f48f317e629faee6f
Rooster <nguyenthoai06011990@gmail.com>
2024-11-26 21:28:59 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker buildx build --platform linux/amd64 -t metafi/metafi-landing-page:0.1.5 --load .
+docker buildx build --platform linux/amd64 -t metafi/metafi-landing-page:0.1.6 --load .
 
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/metafi-landing-page:0.1.5 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/metafi-landing-page:0.1.6 --push .
 
 
-docker push metafi/metafi-landing-page:0.1.5
+docker push metafi/metafi-landing-page:0.1.6
